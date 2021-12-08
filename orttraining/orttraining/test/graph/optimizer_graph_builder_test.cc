97a478469642d732a57ae8836990e9c3a4d8fb3d
Changming Sun <chasun@microsoft.com>
2021-12-08 22:11:42 +0000

    update

--- orttraining/orttraining/test/graph/optimizer_graph_builder_test.cc
+++ orttraining/orttraining/test/graph/optimizer_graph_builder_test.cc
@@ -200,7 +200,7 @@ static void TestOptimizerGraphBuilderWithInitialStates(OptimizerGraphConfig conf
   for (auto& weight_item : opt_initializer_names_map) {
     for (auto& opt_item : weight_item.second) {
       ASSERT_TRUE(graph.GetInitializedTensor(opt_item.second, tensor));
-      assert(tensor != nullptr);
+      ASSERT_NE(tensor, nullptr);
       ASSERT_TRUE(tensor->data_type() == ONNX_NAMESPACE::TensorProto::FLOAT || tensor->data_type() == ONNX_NAMESPACE::TensorProto::INT64);
       if (tensor->data_type() == ONNX_NAMESPACE::TensorProto::FLOAT) {
         VerifyTensorValue(tensor, values[0]);
